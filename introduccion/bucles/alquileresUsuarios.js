fad45e1606c2ab6b369897239ceafb856777ef6f
Juan Decia <juan.decia@gmail.com>
2025-05-01 13:32:51 -0300

    Alquileres usuarios

--- introduccion/bucles/alquileresUsuarios.js
+++ introduccion/bucles/alquileresUsuarios.js
@@ -57,31 +57,81 @@ const usuarios = [
 let indexUsuario = 0;
 
 while (indexUsuario < usuarios.length) {
-
   // Se selecciona el usuario desde la posición del indice del array que se
   // está recorriendo
   const usuario = usuarios[indexUsuario];
 
   console.log("Evaluando el usuario:", usuario.id);
 
-  // inicializamos otro indice dentro del while principal
-  // esto va a ocurrir una vez para cada usuario para recorrer
-  // las publicaciones de las propiedades
+  // Se utiliza otro indice auxiliar para recorrer las publicaciones
+  // de cada usuario donde se encuentre el índice "indexUsuario"
   let indexPublicacion = 0;
 
-  // Bucle para recorrer las publicaciones del usuario
-  while (indexPublicacion < usuario.publicaciones.length) {
-    // Utiliza el indexPublicacion para acceder a la publicación
-    // Chequea con un if si el type es alquiler
-    // si es así mostralo usando console log()
-    if ( usuario.publicaciones[indexPublicacion].type === "alquiler" ) {
-        
+  // Se verifica si el usuario no tiene publicaciones
+  if (usuario.publicaciones.length === 0) {
+    console.log(`El usuario ${usuario.nombre} no tiene publicaciones.`);
+  } else {
+    // Si tiene se utiliza otro bucle para recorrer las publicaciones
+    while (indexPublicacion < usuario.publicaciones.length) {
+      // Se verifica si el tipo de publicación es alquiler
+      if (usuario.publicaciones[indexPublicacion].type === "alquiler") {
+        console.log(
+          `El usuario ${usuario.nombre} tiene una publicación de alquiler: ${usuario.publicaciones[indexPublicacion].title}`
+        );
+      } else {
+        console.log(
+          `El usuario ${usuario.nombre} no alquila, cuenta con propiedad en ${usuario.publicaciones[indexPublicacion].type}.`
+        );
+      }
+
+      // Se incrementa el índice para recorrer la siguiente publicación
+      indexPublicacion++;
     }
-
-    // [Tu lógica va aca]
-
-    indexPublicacion++;
   }
 
   indexUsuario++;
+}
+
+// ####### FOR #######
+// Resolución con for
+
+for ( let i = 0; i < usuarios.length; i++) {
+  const usuario = usuarios[i];
+
+  console.log("Evaluando el usuario: ", usuario.id);
+
+  if ( usuario.publicaciones.length === 0 ) {
+    console.log(`El usuario ${usuario.nombre} no tiene publicaciones.`);
+  } else {
+    for ( let j = 0; j < usuario.publicaciones.length; j++ ) {
+      if ( usuario.publicaciones[j].type === "alquiler" ) {
+        console.log(
+          `El usuario ${usuario.nombre} tiene una publicación de alquiler: ${usuario.publicaciones[j].title}.`
+        );
+      } else {
+        console.log(`El usuario no alquila, cuenta con propiedad en ${usuario.publicaciones[j].type}.`);        
+      }
+    }
+  }
+}
+
+// ####### FOR OF #######
+// Resolución con FOR OF
+
+for ( const usuario of usuarios ) {
+  console.log("Evaluando el usuario: ", usuario.id);
+
+  if ( usuario.publicaciones.length === 0 ) {
+    console.log(`El usuario ${usuario.nombre} no tiene publicaciones.`);
+  } else {
+    for ( const publicacion of usuario.publicaciones ) {
+      if ( publicacion.type === "alquiler" ) {
+        console.log(
+          `El usuario ${usuario.nombre} tiene una publiación de alquiler: ${publicacion.title}.`
+        );
+      } else {
+        console.log(`El usuario no alquila, cuenta con propiedad en ${publicacion.type}.`);        
+      }
+    }
+  }
 }

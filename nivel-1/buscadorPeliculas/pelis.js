5afdf431285e97a3e42b330428ce44c31ebc8096
Juan Decia <juan.decia@gmail.com>
2025-06-27 10:00:58 -0300

    comentarios

--- nivel-1/buscadorPeliculas/pelis.js
+++ nivel-1/buscadorPeliculas/pelis.js
@@ -1,20 +1,30 @@
-// Recibir y procesar pelis de pelis.json
-const fs = require('fs');
+const fs = require('fs'); // Importar Módulo 'fs' para manejar archivos con node
+
+// Carga y transforma el archivo pelis.json en un objeto
 const pelis = JSON.parse(fs.readFileSync(__dirname + '/pelis.json', 'utf-8'));
 
-// Función para obtener todas las películas
+// Verificar si el archivo cargo correctamente
+if (!pelis || !Array.isArray(pelis)) {
+    console.error('Error al cargar las películas desde pelis.json');
+    throw new Error('No se pudo cargar la lista de películas.');
+}
+
+// Función para obtener todas las películas y retornarlas
 const getPelis = () => {
     return pelis;
 };
 
 // Función para ordenar las películas por una propiedad
 const sortPelis = (property) => {
-    // Verificamos si la propiedad existe en al menos una película
+
+    // Verificamos si la propiedad no existe en una película
+    // Los que existen pasan a ser ordenados.
     if (!pelis.some(peli => property in peli)) {
         console.error(`La propiedad "${property}" no existe en las películas.`);
-        return []; // Retornamos un array vacío en caso de error
+        return []; // Retorna array vacío si no encontró ninguna peli
     }
 
+    // Si encuentra la propiedad, ordena por esa propiedad a través de sort
     return pelis.sort((a, b) => {
         if (a[property] < b[property]) return -1;
         if (a[property] > b[property]) return 1;
